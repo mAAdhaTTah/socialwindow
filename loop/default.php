59c3fb72c7d302bb2537ebd9b26725ad1050c4be
mAAdhaTTah <jamesorodig@gmail.com>
2014-11-08 03:41:20 +0100

    Add has_post_thumbnail check to standard posts

--- loop/default.php
+++ loop/default.php
@@ -4,15 +4,15 @@
 <div class="clearfix"></div>
 <?php get_template_part('partials/entry-meta'); ?>
 <div class="entry-content">
-  <div class="entry-thumbnail">
-  <?php $full = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
-  <ul class="clearing-thumbs" data-clearing>
-    <li>
-      <a href="<?php echo $full[0]; ?>">
-        <?php the_post_thumbnail(); ?>
-      </a>
-    </li>
-  </ul>
-  </div>
+  <?php if ( has_post_thumbnail() ) : ?>
+    <?php $full = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
+    <ul class="clearing-thumbs entry-thumbnail" data-clearing>
+      <li>
+        <a href="<?php echo $full[0]; ?>">
+          <?php the_post_thumbnail(); ?>
+        </a>
+      </li>
+    </ul>
+  <?php endif; ?>
   <?php the_content(); ?>
 </div>
