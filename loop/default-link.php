d1850b0904df01b4e82613e94b9107e3bd0c6b28
mAAdhaTTah <jamesorodig@gmail.com>
2014-11-08 09:59:39 -0500

    Rearrange link post styling to put meta info within box

--- loop/default-link.php
+++ loop/default-link.php
@@ -1,13 +1,18 @@
 <div class="entry-content">
   <?php the_content(); ?>
+
+  <footer class="link-meta">
+    <div class="link-info">
+      <h4 class="link-title"><a href="<?php echo get_post_meta( get_the_ID(), '_format_link_url', true ); ?>" target="_blank">
+        <i class="fa fa-link"></i> <?php the_title(); ?>
+      </a></h4>
+      <p class="link-provider">
+        Source: <a href="<?php echo get_post_meta( get_the_ID(), '_url_embedly_provider_url', true ); ?>" target="_blank"><?php echo get_post_meta( get_the_ID(), '_url_embedly_provider_name', true ); ?></a>
+      </p>
+      <small class="link-description"><?php echo get_post_meta( get_the_ID(), '_url_embedly_description', true ); ?></small>
+    </div>
+  </footer>
+
   <?php get_template_part('partials/entry-meta'); ?>
+
 </div>
-<footer class="link-meta">
-  <div class="link-info">
-    <h4 class="link-title"><a href="<?php echo get_post_meta( get_the_ID(), '_format_link_url', true ); ?>" target="_blank">
-      <i class="fa fa-link"></i> <?php the_title(); ?>
-    </a></h4>
-    <p class="link-provider">Source: <a href="<?php echo get_post_meta( get_the_ID(), '_url_embedly_provider_url', true ); ?>" target="_blank"><?php echo get_post_meta( get_the_ID(), '_url_embedly_provider_name', true ); ?></a></p>
-    <small class="link-description"><?php echo get_post_meta( get_the_ID(), '_url_embedly_description', true ); ?></small>
-  </div>
-</footer>
