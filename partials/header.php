43dab45b85379fcabaa057ddb2afe5fbe56b9ff0
mAAdhaTTah <jamesorodig@gmail.com>
2017-08-06 17:10:08 -0400

    Fix nav header

--- partials/header.php
+++ partials/header.php
@@ -60,7 +60,7 @@
 
       <section class="top-bar-section">
         <?php if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'right top-bar-section'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'right'));
         endif; ?>
       </section>
     </nav>
