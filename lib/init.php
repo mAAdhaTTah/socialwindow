0b7a20e96af7ed7039870e616d88049ab07b276c
mAAdhaTTah <jamesorodig@gmail.com>
2019-07-14 19:29:27 -0400

    Register meta with REST API

--- lib/init.php
+++ lib/init.php
@@ -58,5 +58,68 @@ function roots_setup() {
 
   // Tell the TinyMCE editor to use a custom stylesheet
   add_editor_style('/assets/css/editor-style.css');
+
+  register_post_meta('post', '_format_audio_embed', [
+    'type' => 'string',
+    'description' => 'Audio URL for audio formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_format_link_url', [
+    'type' => 'string',
+    'description' => 'Link URL for link formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_url_embedly_retrieved', [
+    'type' => 'boolean',
+    'description' => 'Whether the URL has been retrieved from Embedly',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_url_embedly_provider_url', [
+    'type' => 'string',
+    'description' => 'Provider URL for link formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_url_embedly_provider_name', [
+    'type' => 'string',
+    'description' => 'Provider name for link formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_url_embedly_description', [
+    'type' => 'string',
+    'description' => 'Link description for link formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_format_quote_source_url', [
+    'type' => 'string',
+    'description' => 'Quote source url for quote formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_format_quote_source_name', [
+    'type' => 'string',
+    'description' => 'Quote source name for quote formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
+
+  register_post_meta('post', '_format_video_embed', [
+    'type' => 'string',
+    'description' => 'Video URL for video formats',
+    'single' => true,
+    'show_in_rest' => true,
+  ]);
 }
 add_action('after_setup_theme', 'roots_setup');
